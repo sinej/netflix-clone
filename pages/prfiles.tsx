ecef7c9ccc156172aea32687774ea549bde826b8
shin <sinej1020@gmail.com>
2024-01-26 00:13:41 +0900

    로그인 시, 프로필로 라우터

--- pages/prfiles.tsx
+++ pages/prfiles.tsx
@@ -1,3 +1,23 @@
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession();
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        parmanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 const Profiles = () => {
   return (
     <div>
@@ -5,3 +25,5 @@ const Profiles = () => {
     </div>
   );
 };
+
+export default Profiles;
