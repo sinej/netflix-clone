6f9fd1e00be4a8c9e3fbba6d3420c670817a4c6d
shin <sinej1020@gmail.com>
2024-01-25 23:40:25 +0900

    signout 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import { getSession, signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix clone</h1>
+      <h1 className="text-4xl text-green-500">Netflix clone</h1>
+      <button
+        type="button"
+        className="h-10 w-full bg-white"
+        onClick={() => signOut()}
+      >
+        Logout
+      </button>
     </>
-  )
+  );
 }
