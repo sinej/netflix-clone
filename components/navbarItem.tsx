02bf34ced7273e8767e1f1c445b2362fd74ff261
shin <sinej1020@gmail.com>
2024-01-30 23:14:38 +0900

    navbar 추가

---
+++ components/navbarItem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type NavbarItemProps = {
+  label: string;
+};
+
+const NavbarItem: React.FC<NavbarItemProps> = (props: NavbarItemProps) => {
+  const { label } = props;
+  return (
+    <div className="text-white cursor-pointer hover:text-gray300 transition">
+      {label}
+    </div>
+  );
+};
+
+export default NavbarItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { getSession, signOut } from "next-auth/react";
 import { NextPageContext } from "next";
-import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -19,18 +19,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user, isLoading, error } = useCurrentUser();
   return (
     <>
-      <h1 className="text-4xl text-green-500">Netflix clone</h1>
-      <p className="text-white">Logged in as: {user?.name}</p>
-      <button
-        type="button"
-        className="h-10 w-full bg-white"
-        onClick={() => signOut()}
-      >
-        Logout
-      </button>
+      <Navbar />
     </>
   );
 }
